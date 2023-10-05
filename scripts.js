a4fb3632be77246d9fc4122ade7ffe35a8c5a098
stanciudrg <stanciudragos1337@gmail.com>
2023-10-05 14:51:04 +0300

    Display book information by creating DOM elements on the Book object
    The Book constructor now creates the DOM elements, adds information and classes to the elements, and appends them into the "#books-container" div. This way, the DOM elements can be accessed by the Book objects using "this".

--- scripts.js
+++ scripts.js
@@ -31,4 +31,41 @@ function Book(title, author, pages, readState) {
     this.pages = pages ? Number(pages) : "No";
     this.readState = readState ? true : false;
 
+    this.bookElement = document.createElement('div');
+    this.bookElement.classList.add('book');
+    this.bookElement.dataset.count = libraryArray.length;
+
+    this.titleElement = document.createElement('h3');
+    this.titleElement.classList.add('title');
+    this.titleElement.textContent = this.title;
+    this.bookElement.appendChild(this.titleElement);
+
+    this.authorElement = document.createElement('div');
+    this.authorElement.classList.add('author');
+    this.authorElement.textContent = `By: ${this.author}`;
+    this.bookElement.appendChild(this.authorElement);
+
+    this.pagesElement = document.createElement('div');
+    this.pagesElement.classList.add('pages');
+    this.pagesElement.textContent = `${this.pages} pages`;
+    this.bookElement.appendChild(this.pagesElement);
+
+    this.readStateElement = document.createElement('div');
+    this.readStateElement.classList.add('read-state');
+    this.readStateElement.textContent = this.readState ? "Read" : "Not read yet";
+    this.bookElement.appendChild(this.readStateElement);
+
+    this.readStateToggler = document.createElement('input');
+    this.readStateToggler.classList.add('read-state_toggler');
+    this.readStateToggler.setAttribute("type", "checkbox");
+    this.readState ? this.readStateToggler.checked = true : this.readStateToggler.checked = false;
+    this.bookElement.appendChild(this.readStateToggler);
+
+    this.deleteButton = document.createElement('button');
+    this.deleteButton.classList.add('delete-button');
+    this.deleteButton.textContent = "Remove book";
+    this.bookElement.appendChild(this.deleteButton);
+
+    document.querySelector('#books-container').appendChild(this.bookElement);
+
 }
