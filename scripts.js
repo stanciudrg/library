7d643d056b289f00be6d7cf602df203ad231c993
stanciudrg <stanciudragos1337@gmail.com>
2023-10-05 14:56:28 +0300

    Create toggleReadState method on the Book constructor's prototype
    The function toggles the "Read" state between true and false

--- scripts.js
+++ scripts.js
@@ -1,19 +1,6 @@
 const libraryArray = [];
 const submitButton = document.querySelector(".add-book").addEventListener('click', addBookToLibrary);
 
-function addBookToLibrary(e) {
-
-    e.preventDefault();
-
-    const formData = new FormData(document.querySelector("form"));
-    const bookObject = new Book(formData.get("title"), formData.get("author"), formData.get("pages"), formData.get("read-state"));
-
-    libraryArray.push(bookObject);
-    document.querySelector('dialog').close();
-    document.querySelector('form').reset();
-
-}
-
 const showForm = document.querySelector('.new-book').addEventListener('click', (e) => document.querySelector('dialog').showModal());
 
 const closeForm = document.querySelector('.close-button').addEventListener('click', (e) => {
@@ -67,5 +54,25 @@ function Book(title, author, pages, readState) {
     this.bookElement.appendChild(this.deleteButton);
 
     document.querySelector('#books-container').appendChild(this.bookElement);
+
+}
+
+Book.prototype.toggleReadState = function () {
+
+    this.readState == false ? this.readState = true : this.readState = false;
+    this.readStateElement.textContent = this.readState ? "Read" : "Not read yet";
+
+}
+
+function addBookToLibrary(e) {
+
+    e.preventDefault();
+
+    const formData = new FormData(document.querySelector("form"));
+    const bookObject = new Book(formData.get("title"), formData.get("author"), formData.get("pages"), formData.get("read-state"));
+
+    libraryArray.push(bookObject);
+    document.querySelector('dialog').close();
+    document.querySelector('form').reset();
 
 }
