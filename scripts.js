0f65b19cca65d2d6b5b53fd86c7f294f24be04ae
stanciudrg <stanciudragos1337@gmail.com>
2023-10-05 14:48:12 +0300

    Create addBookToLibrary function and attach it to form Submit button
    The function retrieves and uses the form input values as arguments for the Book constructor, creates a Book object, pushes the Object into the new "libraryArray" array, then closes and resets the form.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+const libraryArray = [];
+const submitButton = document.querySelector(".add-book").addEventListener('click', addBookToLibrary);
+
+function addBookToLibrary(e) {
+
+    e.preventDefault();
+
+    const formData = new FormData(document.querySelector("form"));
+    const bookObject = new Book(formData.get("title"), formData.get("author"), formData.get("pages"), formData.get("read-state"));
+
+    libraryArray.push(bookObject);
+    document.querySelector('dialog').close();
+    document.querySelector('form').reset();
+
+}
+
 const showForm = document.querySelector('.new-book').addEventListener('click', (e) => document.querySelector('dialog').showModal());
 
 const closeForm = document.querySelector('.close-button').addEventListener('click', (e) => {
